00fa82dfcbaebf3066a5b608a2cb0156f1dd4190
ManishSehrawat <manishsehrawat9896@gmail.com>
2022-04-01 10:11:06 +0530

    Project Structure

---
+++ src/main/java/com/ey/wamacademy/capstoneapi/CapstoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ey.wamacademy.capstoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstoneapi/CapstoneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package capstoneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
